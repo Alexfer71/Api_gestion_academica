6a3065075dbfe04e3a155031ecd0621331d56d14
Alexfer71 <alexfrr14@gmail.com>
2026-03-02 17:52:54 -0500

    NUEVO

--- gestionacademica_front/src/app/components/curso/curso.ts
+++ gestionacademica_front/src/app/components/curso/curso.ts
@@ -1,21 +1,52 @@
 import { Component, OnInit } from '@angular/core';
-import { CursoService } from '../../services/curso';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { cursoService } from '../../services/curso';
+import { Curso } from '../../models/curso';
 
 @Component({
   selector: 'app-curso',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './curso.html',
-  styleUrls: ['./curso.css']
+  styleUrl: './curso.css'
 })
 export class CursoComponent implements OnInit {
+  cursos: Curso[] = [];
 
-  constructor(private cursoService: CursoService) {}
+  nuevo: Curso = {
+    descripcionCurso: '',
+    estadoCurso: 'ACTIVO'
+  } as Curso;
+
+  constructor(private cursoService: cursoService) {}
 
   ngOnInit(): void {
-    this.cursoService.getCursos().subscribe({
-      next: (res) => console.log('Cursos desde backend:', res),
-      error: (err) => console.log('Error:', err)
+    this.cargar();
+  }
+
+  cargar(): void {
+    this.cursoService.listar().subscribe({
+      next: (data: Curso[]) => (this.cursos = data),
+      error: (e: any) => console.log(e),
+    });
+  }
+
+  guardar(): void {
+    this.cursoService.crear(this.nuevo).subscribe({
+      next: (_resp: Curso) => {
+        this.nuevo.descripcionCurso = '';
+        this.cargar();
+      },
+      error: (e: any) => console.log(e),
+    });
+  }
+
+  eliminar(id: number): void {
+    this.cursoService.eliminar(id).subscribe({
+      next: () => this.cargar(),
+      error: (e: any) => console.log(e),
     });
   }
 }
--- gestionacademica_front/src/app/services/curso.ts
+++ gestionacademica_front/src/app/services/curso.ts
@@ -1,18 +1,23 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Curso } from '../models/curso';
+import { curso } from '../models/curso';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class CursoService {
-
-  private apiUrl = 'http://localhost:8080/api/curso';
+@Injectable({ providedIn: 'root' })
+export class cursoService {
+  private url = 'http://localhost:8080/api/curso';
 
   constructor(private http: HttpClient) {}
 
-  getCursos(): Observable<Curso[]> {
-    return this.http.get<Curso[]>(this.apiUrl);
+  listar(): Observable<curso[]> {
+    return this.http.get<curso[]>(this.url);
+  }
+
+  crear(c: curso): Observable<curso> {
+    return this.http.post<curso>(this.url, c);
+  }
+
+  eliminar(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.url}/${id}`);
   }
 }
